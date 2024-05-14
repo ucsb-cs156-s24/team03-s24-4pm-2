f700e5135f22443c2f4897b2fa72a71ab9990c3d
Kelly Flippo <kellyflippo@169-231-205-59.wireless.ucsb.edu>
2024-05-13 18:45:40 -0700

    added articles create test

--- frontend/src/tests/pages/Articles/ArticlesCreatePage.test.js
+++ frontend/src/tests/pages/Articles/ArticlesCreatePage.test.js
@@ -8,24 +8,40 @@ import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
+
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+    return {
+        __esModule: true,
+        ...originalModule,
+        Navigate: (x) => { mockNavigate(x); return null; }
+    };
+});
+
 describe("ArticlesCreatePage tests", () => {
 
     const axiosMock = new AxiosMockAdapter(axios);
 
-    const setupUserOnly = () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
         axiosMock.reset();
         axiosMock.resetHistory();
         axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
         axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-    };
-
+    });
+    
     const queryClient = new QueryClient();
-    test("Renders expected content", () => {
-        // arrange
-
-        setupUserOnly();
-       
-        // act
+    test("renders without crashing", () => {
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
@@ -33,11 +49,56 @@ describe("ArticlesCreatePage tests", () => {
                 </MemoryRouter>
             </QueryClientProvider>
         );
-
-        // assert
-        expect(screen.getByText("Create page not yet implemented")).toBeInTheDocument();
     });
 
+    test("on submit, makes request to backend, and redirects to /articles", async () => {
+
+    const queryClient = new QueryClient();
+    const article = {
+        id: 3,
+        title: "Article on Subway",
+        explanation: "all about subway"
+    };
+
+    axiosMock.onPost("/api/articles/post").reply(202, article);
+
+    render(
+        <QueryClientProvider client={queryClient}>
+            <MemoryRouter>
+                <ArticlesCreatePage />
+            </MemoryRouter>
+        </QueryClientProvider>
+    )
+
+    await waitFor(() => {
+        expect(screen.getByLabelText("Title")).toBeInTheDocument();
+    });
+
+    const titleInput = screen.getByLabelText("Title");
+    expect(titleInput).toBeInTheDocument();
+
+    const explanationInput = screen.getByLabelText("Explanation");
+    expect(explanationInput).toBeInTheDocument();
+
+    const createButton = screen.getByText("Create");
+    expect(createButton).toBeInTheDocument();
+
+    fireEvent.change(titleInput, { target: { value: 'Article on Subway' } })
+    fireEvent.change(explanationInput, { target: { value: 'all about subway' } })
+    fireEvent.click(createButton);
+
+    await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
+
+    expect(axiosMock.history.post[0].params).toEqual({
+        title: "South Coast Deli",
+        explanation: "Sandwiches and Salads"
+    });
+
+    // assert - check that the toast was called with the expected message
+    expect(mockToast).toBeCalledWith("New article Created - id: 3 name: Article on Subway");
+    expect(mockNavigate).toBeCalledWith({ "to": "/articles" });
+
+    });
 });
 
 
