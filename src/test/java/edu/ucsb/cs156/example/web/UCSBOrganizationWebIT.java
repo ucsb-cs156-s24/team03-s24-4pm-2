b39fbc0b5cb5491417eff751387d57470c7b4b2f
yusheng <yusheng@ucsb.edu>
2024-06-04 21:12:35 -0700

    Tried to fix timeout error

--- src/test/java/edu/ucsb/cs156/example/web/UCSBOrganizationWebIT.java
+++ src/test/java/edu/ucsb/cs156/example/web/UCSBOrganizationWebIT.java
@@ -21,40 +21,41 @@ public class UCSBOrganizationWebIT extends WebTestCase {
     public void admin_user_can_create_edit_delete_organization() throws Exception {
         setupUser(true);
 
-        page.getByText("UCSBOrganization").click();
+        page.getByText("UCSB Organization").click();
 
-        page.getByText("Create UCSBOrganization").click();
-        assertThat(page.getByText("Create UCSBOrganization")).isVisible();
+        page.getByText("Create UCSB Organization").click();
+        assertThat(page.getByText("Create New UCSB Organization")).isVisible();
         page.getByTestId("UCSBOrganizationForm-orgCode").fill("ZPR");
         page.getByTestId("UCSBOrganizationForm-orgTranslationShort").fill("ZETA");
         page.getByTestId("UCSBOrganizationForm-orgTranslation").fill("ZETA");
-        page.getByTestId("UCSBOrganizationForm-inactive").fill("true");
+        page.getByTestId("UCSBOrganizationForm-inactive").selectOption("true");
         page.getByTestId("UCSBOrganizationForm-submit").click();
 
         assertThat(page.getByTestId("UCSBOrganizationTable-cell-row-0-col-orgTranslationShort"))
                 .hasText("ZETA");
-        assertThat(page.getByTestId("OrganizationTable-cell-row-0-col-orgTranslation"))
+        assertThat(page.getByTestId("UCSBOrganizationTable-cell-row-0-col-orgTranslation"))
                 .hasText("ZETA");
 
         page.getByTestId("UCSBOrganizationTable-cell-row-0-col-Edit-button").click();
         assertThat(page.getByText("Edit UCSBOrganization")).isVisible();
-        page.getByTestId("UCSBOrganizationForm-orgTranslationShort").fill("SKY");
-        page.getByTestId("UCSBOrganizationForm-inactive").fill("false");
+        page.getByTestId("UCSBOrganizationForm-orgTranslationShort").fill("Test");
+        page.getByTestId("UCSBOrganizationForm-orgTranslation").fill("ZETA");
+        page.getByTestId("UCSBOrganizationForm-inactive").selectOption("false");
         page.getByTestId("UCSBOrganizationForm-submit").click();
-        assertThat(page.getByTestId("UCSBOrganizationTable-cell-row-0-col-orgTranslationShort")).hasText("SKY");
-        assertThat(page.getByTestId("OrganizationTable-cell-row-0-col-orgTranslation")).hasText("Test");
-        assertThat(page.getByTestId("OrganizationTable-cell-row-0-col-inactive")).hasText("false");
+        assertThat(page.getByTestId("UCSBOrganizationTable-cell-row-0-col-orgTranslationShort")).hasText("Test");
+        assertThat(page.getByTestId("UCSBOrganizationTable-cell-row-0-col-orgTranslation")).hasText("ZETA");
+        assertThat(page.getByTestId("UCSBOrganizationTable-cell-row-0-col-inactive")).hasText("false");
         page.getByTestId("UCSBOrganizationTable-cell-row-0-col-Delete-button").click();
-        assertThat(page.getByTestId("UCSBOrganizationTable-cell-row-0-col-orgCode")).not().isVisible();
+        assertThat(page.getByTestId("UCSBOrganizationTable-cell-row-0-col-name")).not().isVisible();
     }
 
     @Test
     public void regular_user_cannot_create_organization() throws Exception {
         setupUser(false);
 
-        page.getByText("UCSBOrganization").click();
+        page.getByText("UCSB Organization").click();
 
-        assertThat(page.getByText("Create UCSBOrganization")).not().isVisible();
+        assertThat(page.getByText("Create UCSB Organization")).not().isVisible();
         assertThat(page.getByTestId("UCSBOrganizationTable-cell-row-0-col-orgCode")).not().isVisible();
     }
 }
